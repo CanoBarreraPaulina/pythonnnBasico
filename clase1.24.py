8fa1901bb78c70ae5816847a598b3bd43ea3ae14
Paulina CanBar <canobpaulina@gmail.com>
2023-07-24 23:50:19 +0000

    ejercicio1 del curso

---
+++ clase1.24.py
@@ -0,0 +1,11 @@
+import colorsys
+from colorsys import rgb_to_this 
+
+print(type(10))
+print(type("cadena"))
+print(type(15.4))
+def func():
+    return 0
+print(type(func))
+
+print(type(variable)==(10)._class_)
