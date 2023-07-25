46189888bcb87ec25aed65dc01ed50f51f67cd51
Paulina CanBar <canobpaulina@gmail.com>
2023-07-25 01:40:26 +0000

    3er ejercicio de la primer clase

---
+++ operadores4.py
@@ -0,0 +1,8 @@
+print ("El rango de edad es entre 30 y 39 años")
+edad= int(input("ingrese su edad: "))
+cond1= edad<=39
+cond2= edad>=30
+print(f"Su edad, {edad} años esta dentro del rango de manera")
+print(cond1 and cond2)
+
+
