ec47d99bc43e9a8f31ae3df6355c52de81686f3f
Paulina CanBar <canobpaulina@gmail.com>
2023-07-25 00:47:15 +0000

    operadores ejercicio primera clase

---
+++ operadores2.py
@@ -0,0 +1,6 @@
+base=int(input("Ingrese la base de su rectangulo: "))
+altura=int(input("Ingrese la altura de su rectangulo: "))
+Area= base*altura
+Perimetro= (base+altura)*2
+print("El area en u^2 es:",Area )
+print("El perimetro en u es: ", Perimetro)
