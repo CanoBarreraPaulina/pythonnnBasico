72160d761f86837599a475c15928cf8a94114d36
PaulinaCanBar <111727219+CanoBarreraPaulina@users.noreply.github.com>
2023-08-05 00:02:13 +0000

    proyecto basico, me canse eh

--- Proyecto_CanoBarrera_Paulina/Proyecto_CanoBarrera_Paulina.py
+++ Proyecto_CanoBarrera_Paulina/Proyecto_CanoBarrera_Paulina.py
@@ -1,27 +1,39 @@
 import csv
-def  main():
+continuar=True
+while continuar:
+    i=int(input('''\n*BIENVENIDO A TU CALCULADORA DE GASTOS*
+por favor, ingresa la opcion que desees ejecutar:
+1.Registrar Gasto         2.Registrar Ingreso
+3.Consulta de Saldo       4.Historial de Transacciones
+                   5.Salir\n'''))
     archivo=open("Proyecto_CanoBarrera_Paulina/Gastos.txt", "a+")
     archivo1=open("Proyecto_CanoBarrera_Paulina/Ingresos.txt", "a+")
     if i==1:
         archivo2=open("Proyecto_CanoBarrera_Paulina/GastosDes.txt", "a")
-        Gasto=int(input("Ingrese la cantidad del gasto:\n"))
-        archivo.write(" "+ Gasto)
-        archivo.close()
-        Des=input("Por favor ingrese una descripcion del gasto(sin espacios):\n")
-        archivo2.write(" "+ Des)
-        archivo2.close()
-        print("Gasto Guardado")
-        return main
+        Gasto=input("Ingrese la cantidad del gasto (Solo numeros):\n")
+        Des=input("Por favor ingrese una descripcion del gasto: solo letras, sin espacios(ejemplogasto):\n")
+        if Gasto.isdigit() and Des.isalpha():
+            archivo.write(" "+ Gasto)
+            archivo.close()               
+            archivo2.write(" "+ Des)
+            archivo2.close()
+            print("Gasto Guardado")
+        else:
+            print("Por favor verifique que los datos ingresados sean correctos. Ingrese de nuevo")
+            i==0
     elif i==2:
         archivo2=open("Proyecto_CanoBarrera_Paulina/IngresosDes.txt", "a")
         ingreso=input("Ingrese la cantidad del Ingreso:\n")
-        archivo1.write(" "+ ingreso)
-        archivo1.close()
-        Des=input("Por favor ingrese una descripcion del ingreso (sin espacios):\n")
-        archivo2.write(" "+ Des)
-        archivo2.close()
-        print("Ingreso Guardado")
-        return main
+        Des=input("Por favor ingrese una descripcion del ingreso, sin espacios. (ejemplo_ingreso):\n")
+        if ingreso.isdigit() and Des.isalpha():
+            archivo1.write(" "+ ingreso)
+            archivo1.close()               
+            archivo2.write(" "+ Des)
+            archivo2.close()
+            print("Ingreso Guardado")
+        else:
+            print("Por favor verifique que los datos ingresados sean correctos. Ingrese de nuevo")
+            i==0
     elif i==3:
         dat=[]
         dat2=[]
@@ -35,7 +47,6 @@ def  main():
         datos1=list(map(int, dat))
         lista=sum(datos1)
         archivo.close()
-
         lines = csv.reader(archivo1, delimiter=' ')
         datos2=list(lines)[0]
         for espacio in datos2:
@@ -44,35 +55,36 @@ def  main():
         datos2=list(map(int, dat2))
         lista1=sum(datos2)
         archivo1.close()
+        saldo=lista1-lista
+        if saldo<=0:
+            sald=0
+        else:
+            sald=saldo
         print("Tus ingresos son: ",lista1)
-        print("Tus gastos son: ", lista)
-        print("tu saldo es: ", lista1-lista)
+        print("Tus gastos son: ", lista)           
+        print("tu saldo es: ", sald)
     elif i==4:
         archivo=open("Proyecto_CanoBarrera_Paulina/Gastos.txt", "r")
         archivo1=open("Proyecto_CanoBarrera_Paulina/Ingresos.txt", "r")
         archivo2=open("Proyecto_CanoBarrera_Paulina/GastosDes.txt", "r")
         archivo3=open("Proyecto_CanoBarrera_Paulina/IngresosDes.txt", "r")
-        
         line = csv.reader(archivo, delimiter=' ')
         datos1=list(line)[0]
         lines = csv.reader(archivo2, delimiter=' ')
         datos2=list(lines)[0]
         print("historial de gastos:")
         for indice, valor_a in enumerate(datos1):
-	        print(f"{valor_a} {datos2[indice]}")
-        linee = csv.reader(archivo1, delimiter=' ')
+            print(f"{valor_a} {datos2[indice]}")
+        linee=csv.reader(archivo1, delimiter=' ')
         datos3=list(linee)[0]
         liness = csv.reader(archivo3, delimiter=' ')
         datos4=list(liness)[0]
         print("\n")
         print("historial de Ingresos:")
         for indice, valor_x in enumerate(datos3):
-	        print(f"{valor_x} {datos4[indice]}")
-            return main
-
-
-i=int(input('''BIENVENIDO A TU CALCULADORA DE GASTOS
-por favor, ingresa la opcion que desees ejecutar:
-1.Registrar Gasto         2.Registrar Ingreso
-3.Consulta de Saldo       4.Historial de Transacciones\n'''))
-main() 
+            print(f"{valor_x} {datos4[indice]}")
+    elif i==5:
+        print("Programa cerrado")
+        continuar= False
+    else:
+        print("Por favor, ingresa una opcion correcta")
